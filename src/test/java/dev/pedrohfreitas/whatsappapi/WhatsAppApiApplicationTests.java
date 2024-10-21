5d72a37948f22c9e6a2118c867b30b2787d9930b
Pedro Henrique Freitas <1pedrohfreitas@gmail.com>
2024-10-21 16:17:54 -0300

    Initial commit

---
+++ src/test/java/dev/pedrohfreitas/whatsappapi/WhatsAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pedrohfreitas.whatsappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
